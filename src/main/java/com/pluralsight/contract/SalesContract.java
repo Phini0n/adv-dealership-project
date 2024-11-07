85a11da2e7ea1b3527964f9a5f7edc268bbf91c5
Phini0n <patrayowilson@gmail.com>
2024-11-07 01:50:04 -0500

    Created constructor for SalesContract.

--- src/main/java/com/pluralsight/contract/SalesContract.java
+++ src/main/java/com/pluralsight/contract/SalesContract.java
@@ -5,21 +5,42 @@ import com.pluralsight.dealership.Vehicle;
 import java.math.BigDecimal;
 
 public class SalesContract extends Contract {
+    public static final BigDecimal ONE_HUNDRED = new BigDecimal(100);
+    private final BigDecimal SALES_TAX_AMOUNT = new BigDecimal(5);
+    private final BigDecimal RECORDING_FEE = new BigDecimal(100);
+    private final BigDecimal processingFee;
+    private final Boolean wantsToFinance;
 
-    private final double salesTaxAmount = .05; // 5%
-    // private final BigDecimal recordingFee = 100;
-
-    public SalesContract(String date, String name, String email, Vehicle vehicleSold) {
+    public SalesContract(String date, String name, String email, Vehicle vehicleSold, boolean wantsToFinance) {
         super(date, name, email, vehicleSold);
+        this.processingFee = this.getVehicleSold().getPrice() // If greater than/equal to 10,000 = 495, else 295
+                .compareTo(new BigDecimal(10000)) >= 0 ? new BigDecimal(495) : new BigDecimal(295);
+        this.wantsToFinance = wantsToFinance;
+        this.getTotalPrice();
     }
 
     @Override
     public BigDecimal getTotalPrice() {
-        return null;
+        BigDecimal finalPrice;
+
+        finalPrice = this.getVehicleSold()
+                .getPrice()
+                .add(this.RECORDING_FEE).add(this.processingFee)
+                .add(this.getMonthlyPayment());
+        finalPrice = finalPrice.add(percentage(finalPrice, SALES_TAX_AMOUNT));
+        return finalPrice;
     }
 
     @Override
     public BigDecimal getMonthlyPayment() {
-        return null;
+        if (wantsToFinance) {
+            
+        } else {
+            return null;
+        }
+    }
+
+    public static BigDecimal percentage(BigDecimal base, BigDecimal pct){
+        return base.multiply(pct).divide(ONE_HUNDRED);
     }
 }
